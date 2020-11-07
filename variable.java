2facbd1f061ef1733edbb04dc543dba9948d7854
Levent Elibal <leventelibal@gmail.com>
2020-11-07 16:17:32 -0600

    add test file

--- variable.java
+++ variable.java
@@ -2,15 +2,19 @@ public class variable {
 
     public static void main(String []arg) {
 
-        int a = 9 ;
-        int b = 10 ;
+        int a = 9;
+        int b = 10;
         double c = 9.87;
         float d = 9.86f;
+
+        System.out.println(a+b);
         
         System.out.println(c);
         System.out.println(d);
 
-        boolean e = false;
+        boolean e= false;
+
+        System.out.println(e);
 
         String f = "Grant";
         String g = "is";
