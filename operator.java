a40f1d35b5c36083d99480c296743e41c4fb8a30
Levent Elibal <leventelibal@gmail.com>
2020-11-07 15:33:40 -0600

    add test file

---
+++ operator.java
@@ -0,0 +1,19 @@
+public class operator {
+
+    public static void main(String []args) 
+    
+    {
+     
+        double a =10;
+        double b= 11;
+
+        System.out.println(a/b);
+    
+    
+    String c = "Grant";
+    String d = "is";
+    String e = "awesome";
+
+    System.out.println(c +" "+ d +" " +e);
+    }
+}
---
+++ variable.java
@@ -0,0 +1,21 @@
+public class variable {
+
+    public static void main(String []arg) {
+
+        int a = 9 ;
+        int b = 10 ;
+        double c = 9.87;
+        float d = 9.86f;
+        
+        System.out.println(c);
+        System.out.println(d);
+
+        boolean e = false;
+
+        String f = "Grant";
+
+
+        
+    }
+    
+}
