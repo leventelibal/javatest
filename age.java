2941b9ec4e64b0ef89805ebb634ee1342754b8dd
Levent Elibal <leventelibal@gmail.com>
2020-11-07 16:02:03 -0600

    add test file

---
+++ age.java
@@ -0,0 +1,23 @@
+
+
+public class age {
+
+    public static void main(String []args) {
+        float my_age = 40;
+        float mom_age = 60;
+
+        System.out.println(my_age+mom_age);
+        System.out.println(my_age-mom_age);
+       
+        System.out.println(my_age*mom_age);
+
+        float i = mom_age/my_age;
+
+        System.out.println(i);
+
+        float j = my_age/mom_age;
+
+        System.out.print(j);
+    }
+    
+}
