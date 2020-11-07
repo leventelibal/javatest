76170f81a7423e791f7339b657d72a974300570c
Levent Elibal <leventelibal@gmail.com>
2020-11-07 16:28:51 -0600

    add test file

--- convert.java
+++ convert.java
@@ -2,7 +2,28 @@ public class convert {
 
     public static void main(String []args) {
 
-        
+
+      String a = "3";
+     
+      int b = 4 ;
+
+      int c = Integer.parseInt(a);
+
+
+
+      System.out.println(a+b);
+
+      System.out.println(b+c);
+
+
+      double d = 9;
+
+      String result = Double.toString(d);
+
+      System.out.println(result);
+
+
+
         
     }
     
