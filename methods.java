f32b8284862fd385d9d8963b46bee683c733cd6c
Levent Elibal <leventelibal@gmail.com>
2020-11-07 16:48:57 -0600

    add test file

---
+++ methods.java
@@ -0,0 +1,24 @@
+public class methods {
+
+    public static void main(String []args) {
+
+
+        int a = 3;
+        int b = 4;
+        int c = 5;
+        int d = 6;
+
+       addNumbers(a, b); 
+       addNumbers(c, d); 
+
+    }    
+            public static void addNumbers (int a, int b) {
+
+                    int result = a + b ;
+                    System.out.println(result);
+
+            }
+                
+            
+    
+}
