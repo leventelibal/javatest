36a7fff5cb3b051837caf3b5d30d95ac1e1c8c25
Levent Elibal <leventelibal@gmail.com>
2020-11-09 15:12:18 -0600

    add test file

---
+++ reversestring.java
@@ -0,0 +1,10 @@
+public class reversestring {
+    public String reverse(String s) {
+
+        String result = "";
+        for (int i = 0; i < s.length(); i++) {
+            result += s.charAt(s.length() - i - 1);
+        }
+        return result;
+    }
+}
