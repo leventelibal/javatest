5c83e2a43b856bbacbb3de84b4e6238063656e25
Levent Elibal <leventelibal@gmail.com>
2020-11-09 13:29:17 -0600

    add test file

---
+++ linearshearch.java
@@ -0,0 +1,14 @@
+public class linearshearch {
+    public int search(Integer n, Integer[] list) {
+
+    int index = -1;
+    for(int i = 0;i<list.length;i++)
+    {
+        if (list[i].equals(n)) {
+            index = i;
+            break;
+        }
+    }
+    return index;
+}
+}
